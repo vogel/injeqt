470196dd3353282ae180ceb7b3d0bd5b6f45bc93
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-07-20 17:53:29 +0200

    test new type behaviour
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ test/unit/type-test.cpp
@@ -0,0 +1,58 @@
+/*
+ * %injeqt copyright begin%
+ * Copyright 2014 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %injeqt copyright end%
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "expect.h"
+#include "type.cpp"
+
+#include <QtTest/QtTest>
+
+using namespace injeqt::v1;
+
+class type_test : public QObject
+{
+	Q_OBJECT
+
+private slots:
+	void should_throw_when_created_with_null_meta_object();
+	void should_retun_valid_meta_object();
+
+};
+
+class valid_type : public QObject
+{
+	Q_OBJECT
+};
+
+void type_test::should_retun_valid_meta_object()
+{
+	expect<invalid_type_exception>([]{
+		auto t = type{nullptr};
+	});
+}
+
+void type_test::should_throw_when_created_with_null_meta_object()
+{
+	auto t = type{std::addressof(valid_type::staticMetaObject)};
+
+	QCOMPARE(t.meta_object(), std::addressof(valid_type::staticMetaObject));
+}
+
+QTEST_APPLESS_MAIN(type_test)
+#include "type-test.moc"
