8bad1b6bc2cec9d592e03f61fabac13e9d24be7c
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-07-29 00:42:01 +0200

    add implemented_by_mapping
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ include/implemented-by-mapping.h
@@ -0,0 +1,36 @@
+/*
+ * %injeqt copyright begin%
+ * Copyright 2014 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %injeqt copyright end%
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#pragma once
+
+#include "implemented-by.h"
+#include "injeqt-global.h"
+#include "sorted-unique-vector.h"
+
+namespace injeqt { namespace v1 {
+
+inline type type_from_implemented_by(const implemented_by &i)
+{
+	return i.interface_type();
+}
+
+using implemented_by_mapping = sorted_unique_vector<type, implemented_by, type_from_implemented_by>;
+
+}}
