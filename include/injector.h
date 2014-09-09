e964cbdf41a4c0a325b9b4fd30478d5a2740e4d3
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-09-10 00:59:59 +0200

    add some documentation for injector class
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- include/injector.h
+++ include/injector.h
@@ -27,6 +27,11 @@
 #include <vector>
 #include <QtCore/QObject>
 
+/**
+ * @file
+ * @brief Contains classes and functions for creating injectors.
+ */
+
 namespace injeqt { namespace internal {
 	class injector_impl;
 }}
@@ -35,23 +40,78 @@ namespace injeqt { namespace v1 {
 
 class module;
 
+/**
+ * @brief Injector is created from set of modules that contains injectable types.
+ *
+ * Injector is constructed from set of modules that contains groups of related types.
+ * Then it can be used to get instances of configured types that have their dependencies
+ * resolved and set - there is no need to manually wire services and factories.
+ *
+ * If two or more modules have the same type configured an exception is thrown during construction.
+ * Otherwise, from all types in modules a set of available types is computed. Each type directly
+ * added to modules is added to it. Then all unique super types are added to this set. This means
+ * that two types with common ancestor can be used in injection. Note that Qt meta object system
+ * only supports single inheritance.
+ *
+ * Injector takes ownership of modules passed to it. Also all objects created inside injector
+ * (using default constructor - configured with module::add_type<T>(), or factory - configured
+ * with module::add_factory<T, F>()) are under its ownership. Lifetime of objects added as ready
+ * objects (configured with module::add_ready_object<T>(QObject *) is not managed by injector.
+ * For clarity ready objects can be stored in module instances as unique pointers. Injector will own
+ * then as it own modules.
+ */
 class INJEQT_API injector final
 {
 
 public:
+	/**
+	 * @brief Create empty injector with no types configured.
+	 *
+	 * Empty injector will not be able to produce any object of any type.
+	 */
 	injector();
+
+	/**
+	 * @brief Create new injector from provided modules.
+	 * @param modules list of modules
+	 *
+	 * Creates injector with all types from modules configured. If combined configuration
+	 * of all modules is invalid an exception is thrown. Configuration is invalid when:
+	 * * any type is configured in more than one module
+	 * * a dependency exists with type that is non configured in any module
+	 * * a cycle of factories is found (currently does not throw an exception)
+	 */
 	explicit injector(std::vector<std::unique_ptr<module>> modules);
 	injector(injector &&x);
 	~injector();
 
 	injector & operator = (injector &&x);
 
+	/**
+	 * @brief Returns pointer to object of given type T.
+	 * @param T type of object to return
+	 *
+	 * When object of given type is requested by get<T>() method, injector first check if T is in set of
+	 * available types. If not, an exception is thrown. Next an unique configured type U that implements T
+	 * is found. If object of type U was already created, it is returned. If not, it is created with all
+	 * dependencies it requires. Depending on configuration of U it can be created directly by U default
+	 * constructor or it can be created by factory that is assigned to that type (note: injector will
+	 * create itself all required factories with the same alghoritm). After U with all its dependencies
+	 * is created all dependency setters are called with proper arguments. Then U object is added to cache
+	 * and is itself returned.
+	 */
 	template<typename T>
 	T * get()
 	{
 		return qobject_cast<T *>(get(make_type<T>()));
 	}
 
+	/**
+	 * @brief Returns pointer to object of given type interface_type.
+	 * @param interface_type type of object to return
+	 *
+	 * @see T * get<T>()
+	 */
 	QObject * get(const type &interface_type);
 
 private:
