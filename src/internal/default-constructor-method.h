1c600b2409e0a0b12ac24acba1c7422b1700ebaa
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-09-11 01:19:48 +0200

    add documentation for default_constructor_method
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- src/internal/default-constructor-method.h
+++ src/internal/default-constructor-method.h
@@ -27,6 +27,11 @@
 #include <memory>
 #include <QtCore/QMetaMethod>
 
+/**
+ * @file
+ * @brief Contains classes and functions for representing default constructors.
+ */
+
 class QObject;
 
 using namespace injeqt::v1;
@@ -37,15 +42,57 @@ DEFINE_EXCEPTION(default_constructor_exception, injeqt_exception);
 DEFINE_EXCEPTION(no_default_constructor_exception, default_constructor_exception);
 DEFINE_EXCEPTION(invalid_default_constructor_exception, default_constructor_exception);
 
+/**
+ * @brief Abstraction of default constructor.
+ *
+ * This class is used for creating objects of types configured with module::add_type<T>().
+ * Default constructor is crated from Qt type QMetaMethod. As Qt only creates QMetaMethod
+ * objects for slots, signals and all methods marked with Q_INVOKABLE tag - injeqt
+ * requires that default constructor is also maked with that tag. It is also not allowed
+ * to have any parameters - even with default values.
+ *
+ * Example of valid default constructor:
+ *
+ *     class with_default_constructor : public QObject
+ *     {
+ *         Q_OBJECT
+ *     public:
+ *         Q_INVOKABLE with_default_constructor() {}
+ *     };
+ *
+ * To check if object is valid call validate(const default_constructor_method &).
+ * To extract constructor from a type call make_default_constructor_method<T>().
+ */
 class default_constructor_method final
 {
 
 public:
+	/**
+	 * @brief Create object from QMetaMethod definition.
+	 * @param meta_method Qt meta method that should be a default constructor
+	 */
 	explicit default_constructor_method(QMetaMethod meta_method);
 
+	/**
+	 * @return Type of objects created by default constructor.
+	 *
+	 * May return invalid type if QMetaMethod passed in constructor was no valid.
+	 */
 	const type & object_type() const;
+
+	/**
+	 * @return Qt representation of default constructor.
+	 *
+	 * May return empty value if QMetaMethod passed in constructor was no valid.
+	 */
 	const QMetaMethod & meta_method() const;
 
+	/**
+	 * @return New instance of type of default constructor.
+	 *
+	 * This method can be only called on valid objects. To ensure that default
+	 * constructor is valid call validate(const default_constructor_method &).
+	 */
 	std::unique_ptr<QObject> invoke() const;
 
 private:
@@ -54,13 +101,35 @@ private:
 
 };
 
+/**
+ * @brief Throws an exception if default_constructor_method d is not valid.
+ * @param d default_constructor_method to validate
+ * @throws invalid_default_constructor_exception if backing QMetaMethod is not a constructor
+ * @throws invalid_default_constructor_exception if backing QMetaMethod contains paramers
+ * @throws invalid_type_exception if backing QMetaMethod comes from invalid type
+ *
+ * Call to validate default_constructor_method d. If backing QMetaMethod is invalid, is not
+ * a constructor or requires paramters - an exception is thrown.
+ */
 void validate(const default_constructor_method &d);
 
 bool operator == (const default_constructor_method &x, const default_constructor_method &y);
 bool operator != (const default_constructor_method &x, const default_constructor_method &y);
 
+/**
+ * @see make_default_constructor_method<T>()
+ */
 default_constructor_method make_default_constructor_method(const type &t);
 
+/**
+ * @brief Extract default constructor method from given type T.
+ * @tparam T type to extract constructor from
+ * @throw no_default_constructor_exception if no default constructor is found.
+ *
+ * This method looks for all constructors of type T tagged with Q_INVOKABLE and returns
+ * default_constructor_method based on one without any parameters (even with default
+ * values). If no such constructor is found - no_default_constructor_exception is thrown.
+ */
 template<typename T>
 inline default_constructor_method make_default_constructor_method()
 {
