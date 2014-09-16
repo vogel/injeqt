5dcdcb83f1c12336728fe7f38f9065a3c0345696
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-09-16 23:56:24 +0200

    add implementations documentation
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- src/internal/implementations.h
+++ src/internal/implementations.h
@@ -24,15 +24,34 @@
 #include "injeqt.h"
 #include "sorted-unique-vector.h"
 
+/**
+ * @file
+ * @brief Contains classes and functions for representing set of Injeqt implementation objects.
+ */
+
 using namespace injeqt::v1;
 
 namespace injeqt { namespace internal {
 
+/**
+ * @brief Extract type from implementation for storting purposes.
+ */
 inline type type_from_implementation(const implementation &i)
 {
 	return i.interface_type();
 }
 
+/**
+ * @brief Abstraction of Injeqt set of implementation objects.
+ *
+ * This set is used to represent all currently available implementations. Implementations are sorted
+ * by types and this class only allows one implementation of given type to exists. Set is sorted
+ * by implementation::interface_type() and it can be matched with other type based sets (like dependencies)
+ * using match() function.
+ *
+ * This class is mostly used by injector_impl to store objects created by injector or provided to it
+ * by module::add_ready_object<T>(QObject *).
+ */
 using implementations = sorted_unique_vector<type, implementation, type_from_implementation>;
 
 }}
