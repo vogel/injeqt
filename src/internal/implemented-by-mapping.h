8f67793769bfb6c291d7a988f20db47085d12b3b
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-09-17 00:26:26 +0200

    add documentation for implemented_by_mapping
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- src/internal/implemented-by-mapping.h
+++ src/internal/implemented-by-mapping.h
@@ -24,15 +24,34 @@
 #include "injeqt.h"
 #include "sorted-unique-vector.h"
 
+/**
+ * @file
+ * @brief Contains classes and functions for representing set of Injeqt implemented_by objects.
+ */
+
 using namespace injeqt::v1;
 
 namespace injeqt { namespace internal {
 
+/**
+ * @brief Extract interface type from implemented_by for storting purposes.
+ */
 inline type type_from_implemented_by(const implemented_by &i)
 {
 	return i.interface_type();
 }
 
+/**
+ * @brief Abstraction of Injeqt set of implemented_by objects.
+ *
+ * This set is used to represent all currently available types and its mapping to configured types.
+ * All available types are interfaces that are implemented by configured type and are not conflicting
+ * (interfaces are conflicting when two or more configured types implements them). Only types in
+ * this class are available for injector to return objects of. Relations object are sorted by
+ * interface types that are unique.
+ *
+ * This class is mostly used in type_relations to store all available types.
+ */
 using implemented_by_mapping = sorted_unique_vector<type, implemented_by, type_from_implemented_by>;
 
 }}
