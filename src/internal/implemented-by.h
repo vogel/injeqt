66b6ad56a631563f30cfbb48f89d3e5a4fb065dc
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-09-17 00:23:32 +0200

    add documentation for implemented_by
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- src/internal/implemented-by.h
+++ src/internal/implemented-by.h
@@ -24,19 +24,53 @@
 #include "injeqt.h"
 #include "type.h"
 
+/**
+ * @file
+ * @brief Contains classes and functions for representing "interface is implemented by type" relations.
+ */
+
 using namespace injeqt::v1;
 
 namespace injeqt { namespace internal {
 
+/**
+ * @brief Any exception that can occur when validating or creating implemented_by objects.
+ */
 INJEQT_EXCEPTION(invalid_implemented_by_exception, injeqt_exception);
 
+/**
+ * @brief Represents "interface is implemented by type" relations.
+ *
+ * In Injeqt dependencies between objects are described by types. When an object of some type
+ * T is added to injector using module class then it can be passed as a dependency of type
+ * T or any of T supertypes - excluding only supertypes common with other configured objects.
+ * This class represens such relation. It is mostly used in implemnted_by_mapping set to
+ * represents all types that injector knows and can handle.
+ *
+ * Call validate(const implemented_by &) to check if object is valid.
+ */
 class implemented_by final
 {
 
 public:
+	/**
+	 * @brief Create new instance of implemented_by.
+	 * @param interface_type type of available interface
+	 * @param implementation_type type of object implementing interface
+	 *
+	 * To check if @p implementation_type really implements @p interface_type call
+	 * validate(const implemented_by &).
+	 */
 	explicit implemented_by(type interface_type, type implementation_type);
 
+	/**
+	 * @return type of available interface.
+	 */
 	type interface_type() const;
+
+	/**
+	 * @return type of object implementing interface.
+	 */
 	type implementation_type() const;
 
 private:
@@ -45,6 +79,13 @@ private:
 
 };
 
+/**
+ * @brief Check if implemented_by object is valid.
+ * @param ib object to check
+ * @throw invalid_type_exception when interface_type is invalid
+ * @throw invalid_type_exception when implementation_type is invalid
+ * @throw invalid_implemented_by_exception if implementation_type does not implement interface_type
+ */
 void validate(const implemented_by &ib);
 
 bool operator == (const implemented_by &x, const implemented_by &y);
