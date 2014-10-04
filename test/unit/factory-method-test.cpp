f19e6697fa70ef741c0bd4d6cf64706acfee23a5
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-10-05 00:17:11 +0200

    update tests for factory_method
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- test/unit/factory-method-test.cpp
+++ test/unit/factory-method-test.cpp
@@ -79,6 +79,15 @@ public slots:
 
 };
 
+class invalid_factory : public QObject
+{
+	Q_OBJECT
+
+public slots:
+	Q_INVOKABLE result_object * create_result_object(int) { return new result_object{}; }
+
+};
+
 class factory_method_test : public QObject
 {
 	Q_OBJECT
@@ -87,6 +96,8 @@ private slots:
 	void should_return_empty_when_created_with_not_invokable_factory_method();
 	void should_return_empty_when_created_with_non_unique_factory_method();
 	void should_return_empty_when_created_with_different_type_factory_method();
+	void should_return_empty_when_created_with_invalid_factory_method();
+	void should_create_empty();
 	void should_create_valid_with_invokable_factory_method();
 	void should_create_valid_with_invokable_factory_with_default_parameter_method();
 	void should_create_object_with_factory_method();
@@ -95,46 +106,63 @@ private slots:
 
 void factory_method_test::should_return_empty_when_created_with_not_invokable_factory_method()
 {
-	QVERIFY(make_factory_method(make_type<result_object>(), make_type<factory_not_invokable>()).is_empty());
+	auto f = make_factory_method(make_type<result_object>(), make_type<factory_not_invokable>());
+	QVERIFY(f.is_empty());
 }
 
 void factory_method_test::should_return_empty_when_created_with_non_unique_factory_method()
 {
-	QVERIFY(make_factory_method(make_type<result_object>(), make_type<non_unique_factory>()).is_empty());
+	auto f = make_factory_method(make_type<result_object>(), make_type<non_unique_factory>());
+	QVERIFY(f.is_empty());
 }
 
 void factory_method_test::should_return_empty_when_created_with_different_type_factory_method()
 {
-	QVERIFY(make_factory_method(make_type<other_object>(), make_type<valid_factory>()).is_empty());
+	auto f = make_factory_method(make_type<other_object>(), make_type<valid_factory>());
+	QVERIFY(f.is_empty());
+}
+
+void factory_method_test::should_return_empty_when_created_with_invalid_factory_method()
+{
+	auto f = make_factory_method(make_type<other_object>(), make_type<invalid_factory>());
+	QVERIFY(f.is_empty());
+}
+
+void factory_method_test::should_create_empty()
+{
+	auto f = factory_method{};
+	QVERIFY(f.is_empty());
 }
 
 void factory_method_test::should_create_valid_with_invokable_factory_method()
 {
-	auto c1 = factory_method{make_method<valid_factory>("create_result_object()")};
-	QCOMPARE(c1.object_type(), make_type<valid_factory>());
-	QCOMPARE(c1.result_type(), make_type<result_object>());
+	auto f1 = factory_method{make_method<valid_factory>("create_result_object()")};
+	QVERIFY(!f1.is_empty());
+	QCOMPARE(f1.object_type(), make_type<valid_factory>());
+	QCOMPARE(f1.result_type(), make_type<result_object>());
 
-	auto c2 = make_factory_method(make_type<result_object>(), make_type<valid_factory>());
-	QCOMPARE(c2.object_type(), make_type<valid_factory>());
-	QCOMPARE(c2.result_type(), make_type<result_object>());
+	auto f2 = make_factory_method(make_type<result_object>(), make_type<valid_factory>());
+	QVERIFY(!f2.is_empty());
+	QCOMPARE(f2.object_type(), make_type<valid_factory>());
+	QCOMPARE(f2.result_type(), make_type<result_object>());
 
-	QCOMPARE(c1, c2);
+	QCOMPARE(f1, f2);
 }
 
 void factory_method_test::should_create_valid_with_invokable_factory_with_default_parameter_method()
 {
-	auto c = make_factory_method(make_type<result_object>(), make_type<valid_factory_with_default_parameter>());
-	QCOMPARE(c.object_type(), make_type<valid_factory_with_default_parameter>());
-	QCOMPARE(c.result_type(), make_type<result_object>());
+	auto f = make_factory_method(make_type<result_object>(), make_type<valid_factory_with_default_parameter>());
+	QVERIFY(!f.is_empty());
+	QCOMPARE(f.object_type(), make_type<valid_factory_with_default_parameter>());
+	QCOMPARE(f.result_type(), make_type<result_object>());
 }
 
 void factory_method_test::should_create_object_with_factory_method()
 {
-	auto factory = make_factory_method(make_type<result_object>(), make_type<valid_factory>());
+	auto f = make_factory_method(make_type<result_object>(), make_type<valid_factory>());
 	auto factory_object = make_object<valid_factory>();
-	auto object = factory.invoke(factory_object.get());
+	auto object = f.invoke(factory_object.get());
 	auto cast = qobject_cast<result_object *>(object.get());
-
 	QVERIFY(cast != nullptr);
 }
 
