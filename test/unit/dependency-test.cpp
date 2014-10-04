107a5d5ee928faafa09d7923e100cabb359b84a0
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-10-04 23:05:41 +0200

    add test for dependency class
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ test/unit/dependency-test.cpp
@@ -0,0 +1,71 @@
+/*
+ * %injeqt copyright begin%
+ * Copyright 2014 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %injeqt copyright end%
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "exception/empty-type-exception.cpp"
+#include "exception/exception.cpp"
+#include "exception/interface-not-implemented-exception.cpp"
+#include "exception/invalid-qobject-exception.cpp"
+#include "exception/qobject-type-exception.cpp"
+#include "dependency.cpp"
+#include "extract-interfaces.cpp"
+#include "setter-method.cpp"
+#include "type.cpp"
+
+#include "make-setter-method.h"
+#include "utils.h"
+
+#include <QtTest/QtTest>
+
+using namespace injeqt::internal;
+using namespace injeqt::v1;
+
+class type_1 : public QObject
+{
+	Q_OBJECT
+};
+
+class type_with_dependency : public QObject
+{
+	Q_OBJECT
+
+public slots:
+	INJEQT_SETTER void setter_1(type_1 *) { }
+
+};
+
+class dependency_test : public QObject
+{
+	Q_OBJECT
+
+private slots:
+	void should_create_valid_dependency();
+
+};
+
+void dependency_test::should_create_valid_dependency()
+{
+	auto s = make_setter_method<type_with_dependency>("setter_1(type_1*)");
+	auto d = dependency{s};
+	QCOMPARE(d.required_type(), make_type<type_1>());
+	QCOMPARE(d.setter(), s);
+}
+
+QTEST_APPLESS_MAIN(dependency_test)
+#include "dependency-test.moc"
